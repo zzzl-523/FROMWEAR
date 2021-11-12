22865611af54b9c87b188af0be67f4a78392b2a1
zzzl-523 <zzh23@naver.com>
2021-11-13 02:01:52 +0900

    weekly_tag_page)outline

---
+++ weekly_tag_page/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import WeeklyTag from './WeeklyTagPage/WeeklyTagPage';
+
+function App() {
+  return (
+	<WeeklyTag />
+  );
+}
+
+export default App;
