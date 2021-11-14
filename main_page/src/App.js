f12364dc1418624912d67308e5030551edaa04e0
zzzl-523 <zzh23@naver.com>
2021-11-14 10:44:20 +0900

    main_page)outline

---
+++ main_page/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainPage from './MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      	<MainPage />
+			</header>
+    </div>
+  );
+}
+
+export default App;
